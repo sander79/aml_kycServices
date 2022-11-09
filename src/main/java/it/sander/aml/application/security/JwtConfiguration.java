5a8eb7b97fe8b14479e4bf74e4b0e9496ff86b4a
Harry <Harry@host.docker.internal>
2022-11-09 18:00:16 +0100

    refactoring

--- src/main/java/it/sander/aml/application/security/JwtConfiguration.java
+++ src/main/java/it/sander/aml/application/security/JwtConfiguration.java
@@ -1,17 +1,37 @@
 package it.sander.aml.application.security;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.GenericFilterBean;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 @Configuration
 public class JwtConfiguration {
@@ -30,14 +50,10 @@ public class JwtConfiguration {
         }
     }
     
-    
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
-        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
-        source.registerCorsConfiguration("/**", corsConfiguration);
-
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
         return source;
     }
     
@@ -45,7 +61,7 @@ public class JwtConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
        	
         http.cors().and().authorizeRequests()
-        .antMatchers("/v2/api-docs", "/configuration/**", "/swagger-resources/**",  "/swagger-ui.html", "/webjars/**", "/api-docs/**").permitAll()
+        .antMatchers("/v2/api-docs", "/configuration/**", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/api-docs/**").permitAll()
         .anyRequest().authenticated()
         .and()
         .addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
@@ -53,6 +69,47 @@ public class JwtConfiguration {
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
  
         return http.build();
+    }
+    
+    
+    // JWTAuthorizationFilter
+    public class JWTAuthorizationFilter extends GenericFilterBean {
+		
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+    			throws IOException, ServletException {
+        	
+            final HttpServletRequest request = (HttpServletRequest) servletRequest;
+            final String authHeader = request.getHeader(JwtConfiguration.headerParam);
+
+            if (authHeader == null || !authHeader.startsWith(JwtConfiguration.prefix)) {
+            	filterChain.doFilter(request, servletResponse);
+                return;
+            }
+     
+            String token = authHeader.substring(JwtConfiguration.prefix.length()+1);
+            Authentication authentication = null;
+    		try {
+    			authentication = getAuthentication(token);
+    		} catch (Exception e) {
+    			e.printStackTrace();
+    		}
+    		
+    		SecurityContextHolder.getContext().setAuthentication(authentication);
+            filterChain.doFilter(request, servletResponse);
+        }
+
+        // Reads the JWT from the Authorization header, and then uses JWT to validate the token
+    	private UsernamePasswordAuthenticationToken getAuthentication(String token) throws Exception {
+    		DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC256(JwtConfiguration.secret)).build().verify(token);	
+    		Claim rolesClaim = decodedJWT.getClaim("roles");
+    		Collection<SimpleGrantedAuthority> authorities = new ArrayList<SimpleGrantedAuthority>();
+    		for(String r : rolesClaim.asList(String.class)) {
+    			authorities.add(new SimpleGrantedAuthority(r));
+    		}		
+    		return new UsernamePasswordAuthenticationToken(decodedJWT.getSubject(), null, authorities);
+    	}
+
     }
     
 }
